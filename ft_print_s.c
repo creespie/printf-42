ff013dbe7977d5a94173a0e10cd6b4d20ad2c71a
creespie <150524904+creespie@users.noreply.github.com>
2026-03-06 12:37:05 +0100

    finish case s

--- ft_print_s.c
+++ ft_print_s.c
@@ -3,54 +3,67 @@
 void	ft_print_s(va_list args, t_flags *flags, int *len)
 {
 	char	*string;
-	int		i;
 
-	i = 0;
-	*string = va_arg(args, char*);
+	string = va_arg(args, char*);
 	if (flags -> width > ft_strlen(string) || (flags -> width > flags -> prec 
 			&& flags -> prec < ft_strlen(string) && flags -> prec > 0))
 	{
 		if (flags -> minus > 0)
 		{
-			if (flags -> prec < ft_strlen(string)) //stringa
-			{
-				while(i < flags -> prec)
-				{
-					ft_putchar(string[i]);
-					i++;
-				}
-				*len = *len + flags -> prec;
-			}
-			else
-			{
-				ft_putstr(string);
-				*len = *len + ft_strlen(string);
-			}
-			if (flags -> prec < ft_strlen(string)) //spazi
-			{
-				while (flags -> width > flags -> prec)
-				{
-					ft_putchar(' ');
-					flags ->width--;
-				}
-			}
-			else
-			{
-				while (flags -> width > ft_strlen(string))
-				{
-					ft_putchar(' ');
-					flags ->width--;
-				}
-			}
+			ft_print_string(flags, string, len);
+			ft_print_spaces(flags, string, len);
 		}
 		else
 		{
-			//spazi
-			//stringa
+			ft_print_spaces(flags, string, len);
+			ft_print_string(flags, string, len);
 		}
 	}
 	else
 	{
-		//stringa
+		ft_print_string(flags, string, len);
+	}
+}
+
+void static	ft_print_string(t_flags *flags, char *string, int *len)
+{
+	int	i;
+
+	i = 0;
+	if (flags -> prec < ft_strlen(string)) 
+	{
+		while(i < flags -> prec)
+		{
+			ft_putchar(string[i]);
+			i++;
+		}
+		*len = *len + flags -> prec;
+	}
+	else		
+	{
+		ft_putstr(string);
+		*len = *len + ft_strlen(string);
+	}
+}
+
+void static	ft_print_spaces(t_flags *flags, char *string, int *len)
+{
+	if (flags -> prec < ft_strlen(string))
+	{
+		while (flags -> width > flags -> prec)
+		{
+			ft_putchar(' ');
+			flags ->width--;
+			(*len)++;
+		}
+	}
+	else
+	{
+		while (flags -> width > ft_strlen(string))
+		{
+			ft_putchar(' ');
+			flags ->width--;
+			(*len)++;
+		}
 	}
 }
